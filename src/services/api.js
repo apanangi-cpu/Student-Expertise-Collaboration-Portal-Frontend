9fb98943900ac8f865ca7b47194752b855cd5fc6
AbhishiekPanangipalli <apanangi@gitam.in>
2026-03-23 14:47:45 +0530

    updated API baseURL with /api

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,28 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "https://student-expertise-collaboration-portal.onrender.com",
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     req.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return req;
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-expertise-collaboration-portal.onrender.com",
+  baseURL: "https://student-expertise-collaboration-portal.onrender.com/api", // ✅ added /api
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
